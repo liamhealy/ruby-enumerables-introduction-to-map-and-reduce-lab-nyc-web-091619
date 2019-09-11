0129e8104f91be020f24f1993d3521ff869837e8
Liam Healy <lhealy0711@gmail.com>
2019-09-11 17:52:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,3 +22,13 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array.push(source_array[i] ** 2)
+    i += 1
+  end
+  new_array
+end
